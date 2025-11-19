22a6d2449c6baec8f16ce7ca06cce63a406c7372
a1070590028-sys <a1070590028@gmail.com>
2025-11-19 11:08:26 +0800

    Add files via upload

---
+++ js/modules/vanta-bg.js
@@ -0,0 +1,12 @@
+VANTA.NET({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200,
+    minWidth: 200,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x60a5fa,
+    backgroundColor: 0x0f172a
+});
