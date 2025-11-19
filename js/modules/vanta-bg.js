fe988b4e78fc11a7e221cc8609ffaedca68db5c4
a1070590028-sys <a1070590028@gmail.com>
2025-11-19 10:43:11 +0800

    Update vanta-bg.js

--- js/modules/vanta-bg.js
+++ js/modules/vanta-bg.js
@@ -1,18 +1,13 @@
-// js/modules/vanta-bg.js
-document.addEventListener("DOMContentLoaded", () => {
-    VANTA.NET({
-        el: document.getElementById("vanta-bg"),
-        mouseControls: true,
-        touchControls: true,
-        gyroControls: false,
-        minHeight: 200,
-        minWidth: 200,
-        scale: 1.0,
-        scaleMobile: 1.0,
-        color: 0x60a5fa,
-        backgroundColor: 0x0f172a,
-        points: 12,
-        maxDistance: 22,
-        spacing: 16
-    });
+VANTA.NET({
+    el: document.getElementById("vanta-bg"),
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200,
+    minWidth: 200,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    color: 0x60a5fa,
+    backgroundColor: 0x0f172a
 });
+
