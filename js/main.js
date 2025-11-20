cfb4a5216711acf77e13131b49123dd882a4c4a1
a1070590028-sys <a1070590028@gmail.com>
2025-11-20 08:47:30 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
 // js/main.js
 import './modules/time.js';
+import './modules/vanta-bg.js';
 import './modules/card-toggle.js';
 import './modules/image-compress.js';
 import './modules/net-monitor.js';
-import './modules/music-player.js';
 import './modules/pdf-convert.js';
-import './modules/vanta-bg.js';
-
-
-
+import './modules/music-player.js';
